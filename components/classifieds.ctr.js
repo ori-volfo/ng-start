ee20539d8903179d0cf4bd4a6132e1da36761f45
ori-volfo <ori.volfo@gmail.com>
2017-11-30 09:05:02 +0200

    building ui router

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,11 +3,13 @@
 
     angular
         .module("ngClassifieds")
-        .controller("classifiedsCtrl",function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast){
-
+        .controller("classifiedsCtrl",function($scope, $http, classifiedsFactory, $mdDialog ,$mdSidenav, $mdToast){
+            
             classifiedsFactory.getClassifieds().then(function(classifieds){
                 $scope.classifieds = classifieds.data;
+                $scope.categories = getCategories($scope.classifieds);
             });
+
 
             var contact = {
                 name: "Ori Volfovitch",
@@ -28,13 +30,58 @@
                     $scope.classifieds.push(classified);
                     $scope.classified = {};
                     $scope.closeSidebar();
-                    $mdToast.show(
-                        $mdToast.simple()
-                            .content("Classified Saved")
-                            .position('top, right')
-                            .hideDelay(2000)
-                    );
+                    showToast("Classified Saved");
+                    
                 }
             }
-        })
+
+            $scope.editClassified = function(classified){
+                $scope.editing = true;
+                $scope.openSidebar();
+                $scope.classified = classified;
+            }
+
+            $scope.saveEdit = function (){
+                $scope.editing = false;
+                $scope.classified = {};
+                $scope.closeSidebar();
+                showToast("Edit Saved");
+            }
+
+            $scope.deleteClassified = function(event, classified){
+                var confirm = $mdDialog.confirm()
+                    .title('Are you sure you want to delete '+classified.title+'?')
+                    .ok('Yes')
+                    .cancel('No')
+                    .targetEvent(event);
+                $mdDialog.show(confirm).then(function(){
+                    var index = $scope.classifieds.indexOf(classified);
+                    $scope.classifieds.splice(index, 1);
+                }, function(){
+
+                });
+                
+            }
+
+            function showToast(message){
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content(message)
+                        .position('top, right')
+                        .hideDelay(2000)
+                );
+            }
+
+            function getCategories(classifieds){
+                var categories = [];
+                angular.forEach(classifieds, function(item) {
+                    angular.forEach(item.categories, function(category){
+                        categories.push(category);
+                    });
+                });
+
+                return _.uniq(categories);
+            }
+
+        });
 })();
--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,29 @@
 angular
-    .module("ngClassifieds",["ngMaterial"])
-    .config(function($mdThemingProvider){
+    .module("ngClassifieds",["ngMaterial","ui.router"])
+    .config(function($mdThemingProvider, $stateProvider, $locationProvider){
         $mdThemingProvider
             .theme('default')
             .primaryPalette('teal')
             .accentPalette('orange');
+        $locationProvider.hashPrefix('');
+        $stateProvider
+        .state('one',{
+            url: '/stateone',
+            template:'<h1>state one</h1>'
+        })
+        .state('two',{
+            url: '/statetwo',
+            template:'<h1>state two</h1><md-button ui-sref="two.more">go to nested state</md-button><ui-view></ui-view>'
+        })
+        .state('two.more',{
+            url: '/more',
+            template: '<p>deeper state</p>'
+        });
     })
     .directive("helloWorld",function(){
         return {
             template: "<h1>{{message}}</h1>"
         }
+
+        
     });
